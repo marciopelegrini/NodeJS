0b6025a654ebd3bf34ad9c97bdacf9ee966b2212
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2022-10-25 16:30:04 -0400

    mvc

--- 10_MVC/controllers/TaskController.js
+++ 10_MVC/controllers/TaskController.js
@@ -5,6 +5,17 @@ module.exports = class TaskController {
         res.render('tasks/create')
     }
 
+    static async createTaskSave(req, res) {
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+
+        await Task.create(task)
+        res.redirect('/tasks')
+    }
+
     static showTasks(req, res) {
         res.render('tasks/all')
     }
